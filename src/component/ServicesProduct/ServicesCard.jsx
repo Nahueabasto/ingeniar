d9ad2d4fc6f08f72ab5b2b18023c68496cd9a54b
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2024-04-02 16:14:33 -0300

    d

--- src/component/ServicesProduct/ServicesCard.jsx
+++ src/component/ServicesProduct/ServicesCard.jsx
@@ -2,35 +2,41 @@ import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import "./ServicesCard.css";
 
-const ServicesCard = (props) => {
-  const [hovered, setHovered] = useState(false);
-  const primeraImagen = props.imgsrc[0];
 
-  console.log("ID capturado en Card:", props.id); // Agrega este console.log
-
-  return (
-    <div className="project-card" style={{ position: 'relative' }}>
-      <Link to={`/serviciosdetalle/${props.id}`} style={{ textDecoration: "none" }}>
-      <img
-          src={primeraImagen}
-          alt="imagen"
-          style={{
-            width: '350px',
-            height: 'auto',
-            // opacity: hovered ? 1 : 0.8,
-            // transition: 'opacity 0.3s ease',
-          }}
-          onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
-        />
-        <p className="project-title" style={{ position: 'absolute', top: '80%', left: '50%', transform: 'translate(-50%, -50%)', padding: '0px', color: 'white', fontSize: '20px', textAlign: 'center', width: '90%' }}>{props.title}</p>
-      </Link>
-      <div className="pro-details">
-        <div>
+  const ServicesCard = (props) => {
+    const [hovered, setHovered] = useState(false);
+    const primeraImagen = props.imgsrc[0];
+  
+    return (
+      <div className="project-card" style={{ position: 'relative' }}>
+        <Link to={`/serviciosdetalle/${props.id}`} style={{ textDecoration: "none" }}>
+          <img
+            src={primeraImagen}
+            alt="imagen"
+            style={{
+              width: '350px',
+              height: 'auto',
+              opacity: hovered ? 0.3 : 1, // Cambia la opacidad cuando el mouse está encima
+            }}
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+          />
+          {hovered && (
+            <div // Contenedor del texto con evento onMouseEnter para mantener la opacidad de la imagen
+              onMouseEnter={() => setHovered(true)}
+              onMouseLeave={() => setHovered(false)}
+              style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', padding: '0px', color: 'black', fontSize: '20px', textAlign: 'center', width: '90%' }}
+            >
+              <p className="project-title">{props.title}</p>
+            </div>
+          )}
+        </Link>
+        <div className="pro-details">
+          <div>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
-export default ServicesCard;
+    );
+  };
+  
+  export default ServicesCard;
