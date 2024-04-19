2e192f70c2e59ff3628cf2f49bb4bd324b057f11
Nahueabasto <85427134+Nahueabasto@users.noreply.github.com>
2024-04-19 09:23:19 -0300

    fs

--- src/component/ServicesProduct/Services.jsx
+++ src/component/ServicesProduct/Services.jsx
@@ -6,8 +6,6 @@ const Services = () => {
   return (
     <div className="subtitle-containe">   
         <h1>Servicios</h1>
-        {/* <p>Brindamos los mejores resultados.
-        </p> */}
         <p>
         </p>
     </div>
